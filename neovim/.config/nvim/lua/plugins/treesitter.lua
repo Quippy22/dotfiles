e8062e3561803889a662807c2e8e9bd30d62afcd
darius alexandru <darius.alexandru2202@gmail.com>
2025-10-20 09:56:01 +0300

    improved treesitter config to install languages

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,31 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = "master",
-        lazy = false,
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  event = 'BufReadPre',
+
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = {
+        'c',
+        'cpp',
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+        'python',
+        'javascript',
+        'html',
+      },
+
+      -- Install parsers synchronously (only for parsers in ensure_installed)
+      sync_install = false,
+
+      -- Automatically install missing parsers (only for parsers in ensure_installed)
+      auto_install = true,
+
+      highlight = {
+        enable = true,
+      },
+    })
+  end,
 }
