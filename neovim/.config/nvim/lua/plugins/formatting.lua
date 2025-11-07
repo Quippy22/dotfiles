6e95da8712eafe55644f9821292c9ea7fefe8c60
darius alexandru <darius.alexandru2202@gmail.com>
2025-11-07 12:23:29 +0200

    neovim: fixed formating... again...

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -1,25 +1,18 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    ft = { "rust", "lua", "python", "c", "cpp" },
-    cmd = { "ConformInfo" },
-    config = function()
-        print("--- Loading conform.nvim configuration (NO format on save) ---")
-
-        local conform = require("conform")
-
-        conform.setup({
-            formatters_by_ft = {
-                rust = { "rustfmt" },
-                lua = { "stylua --indent-width 4 --indent-type Spaces" },
-                python = { "isort", "black --line-length 120" },
-                c = { "clang_format --style={BasedOnStyle: LLVM, IndentWidth: 4, UseTab: Never}" },
-                cpp = { "clang_format --style={BasedOnStyle: LLVM, IndentWidth: 4, UseTab: Never}" },
-            },
-        })
-
-        vim.keymap.set("n", "<leader>gf", function()
-            conform.format({ async = true, lsp_fallback = true })
-        end, { desc = "Format buffer [conform]" })
-    end,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			rust = { "rustfmt" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+		},
+	},
+	init = function()
+		vim.keymap.set("n", "<leader>gf", function()
+			require("conform").format({ async = true, lsp_fallback = true })
+		end, { desc = "Format buffer [conform]" })
+	end,
 }
