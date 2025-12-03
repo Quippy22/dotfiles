217a5bb31f9ca7ed99ef5f618fcdaf315a947768
darius alexandru <darius.alexandru2202@gmail.com>
2025-12-03 10:09:34 +0200

    waybar: fixed the update counter

--- waybar/scripts/updates.sh
+++ waybar/scripts/updates.sh
@@ -3,16 +3,19 @@
 # Get list of available updates
 updates=$(yay -Qua 2>/dev/null)
 
-count=$(echo "$updates" | wc -l)
+if [ -z "$updates" ]; then
+  count=0
+else
+  count=$(echo "$updates" | wc -l)
+fi
 
 if [ "$count" -gt 0 ]; then
   # Format tooltip: escape quotes and replace newlines with \n
-  tooltip=$(echo "$updates" | cut -d' ' -f1 | sed 's/"/\\"/g' | awk '{printf "%s\\n", $0}')
+  tooltip=$(echo "$updates" | cut -d' ' -f1 | sed 's/"/\"/g' | awk '{printf "%s\n", $0}')
 
   # Output valid JSON for Waybar
   echo "{\"text\": \"↑ $count\", \"tooltip\": \"$tooltip\"}"
 else
-  # Nothing to show
-  echo ""
+  # No updates, show 0
+  echo "{\"text\": \"↑ 0\", \"tooltip\": \"No updates\"}"
 fi
-
