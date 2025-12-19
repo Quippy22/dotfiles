0420bb217a9ab59b7fc9c4242ebbd822ef44562c
darius alexandru <darius.alexandru2202@gmail.com>
2025-12-19 15:34:49 +0200

    neovim: auto source .venvs

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,3 +1,20 @@
+local venv_path = vim.fn.getcwd() .. "/.venv"
+if vim.fn.isdirectory(venv_path) == 1 then
+	vim.env.VIRTUAL_ENV = venv_path
+	vim.env.PATH = venv_path .. "/bin:" .. vim.env.PATH
+end
+
+vim.api.nvim_create_autocmd("DirChanged", {
+	pattern = "*",
+	callback = function()
+		local venv = vim.fn.getcwd() .. "/.venv"
+		if vim.fn.isdirectory(venv) == 1 then
+			vim.env.VIRTUAL_ENV = venv
+			vim.env.PATH = venv .. "/bin:" .. vim.env.PATH
+		end
+	end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
