cba3fd7da64c0a156d57a77e01c3a0584699cb4d
Quippy22 <darius.alexandru2202@gmail.com>
2026-02-27 12:46:25 +0200

    zellij: set up

--- neovim/.config/nvim/lua/plugins/smart-splits.lua
+++ neovim/.config/nvim/lua/plugins/smart-splits.lua
@@ -2,6 +2,8 @@ return {
     "mrjones2014/smart-splits.nvim",
     config = function()
         require("smart-splits").setup({
+            -- Multiplexer support
+            multiplexer_integration = "zellij",
             -- Only enable resizing
             ignored_filetypes = {},
             default_amount = 3,
@@ -15,6 +17,11 @@ return {
 
         -- Keymaps 
         local ss = require("smart-splits")
+        vim.keymap.set("n", "<C-h>", ss.move_cursor_left)
+        vim.keymap.set("n", "<C-j>", ss.move_cursor_down)
+        vim.keymap.set("n", "<C-k>", ss.move_cursor_up)
+        vim.keymap.set("n", "<C-l>", ss.move_cursor_right)
+
         vim.keymap.set("n", "<C-Up>", ss.resize_up, { desc = "Resize up" })
         vim.keymap.set("n", "<C-Down>", ss.resize_down, { desc = "Resize down" })
         vim.keymap.set("n", "<C-Left>", ss.resize_left, { desc = "Resize left" })
